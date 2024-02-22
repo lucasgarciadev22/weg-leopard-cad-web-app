a2361b8d8a042fe2ee4eb265266f8a1faa2c531c
Lucas Miguel Garcia da Silva <lucas10@weg.net>
2024-02-22 15:03:22 -0300

    added info

--- weg-leopard-cad-web-app/src/services/IconServices.ts
+++ weg-leopard-cad-web-app/src/services/IconServices.ts
@@ -1,6 +1,7 @@
 import arcc from "../images/tool-icons/arcc.png";
 import line from "../images/tool-icons/line.png";
 
+//importar demais icones conforme popular site, e fazer o mesmo processo
 export function getIcon(iconName: string) {
   switch (iconName) {
     case "arcc":
